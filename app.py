279de1c40d85daf380e4f38b29dc2a551827edbe
sakeena12 <31855499+sakeena12@users.noreply.github.com>
2018-08-10 12:11:36 -0400

    Add basic auth, decorator function to handle

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask, request, url_for, jsonify
+from flask import Flask, request, url_for, jsonify, make_response
+from functools import wraps
+import os
 import tasks
 
 app = Flask(__name__)
 
+password = os.environ['BASIC_AUTH_TOKEN']
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated():
+        auth = request.authorization
+        if auth and auth.password == password:
+            return f()
+
+        return {'Failed to authorize your credentials', 401, jsonify({})}
+
+    return decorated
+
+
 def get_app():
     return app
 
+
 @app.route('/', methods=['GET'])
+@auth_required
 def hello():
     return 'Server is up.'
 
+
 @app.route('/ocr', methods=['POST'])
+@auth_required
 def do_ocr():
     data = request.get_json()
     task = tasks.perform_ocr.delay(data)
